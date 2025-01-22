8b7ed7171f96b19cfc1ca0aa4b5518b5bb1b4450
Joe Simonin <jsimonin95@gmail.com>
2025-01-22 16:57:29 -0500

    Add project files.

---
+++ JosephSimonin_Assignmnet2.py
@@ -0,0 +1,14 @@
+# Average weight of 4 Lobsters
+print("Welcome to Assignment 2: Average weight of 4 lobsters in pounds.")
+
+#Get weights of lobsters
+lobster1 = float(input("Please enter the weight of the 1st lobster in lbs: "))
+lobster2 = float(input("Please enter the weight of the 2nd lobster in lbs: "))
+lobster3 = float(input("Please enter the weight of the 3rd lobster in lbs: "))
+lobster4 = float(input("Please enter the weight of the 4th lobster in lbs: "))
+
+#Calculate Average of lobsters
+Average = (lobster1 + lobster2 + lobster3 + lobster4) / 4
+
+#Output Average
+print(f"The average weight of the 4 entered lobster weights are {Average}lbs")
